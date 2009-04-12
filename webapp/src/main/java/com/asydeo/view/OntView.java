a161bf10a70408392453a5a0808f3126fd830f6f
brianboyd88 <brianboyd88@ba9bcc8e-eef3-11dd-9639-657c987aacbe>
2009-04-12 02:26:08 +0000

    Display individual's class in search results

--- webapp/src/main/java/com/asydeo/view/OntView.java
+++ webapp/src/main/java/com/asydeo/view/OntView.java
@@ -2,12 +2,17 @@ package com.asydeo.view;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntProperty;
 import com.hp.hpl.jena.ontology.OntResource;
 import com.hp.hpl.jena.rdf.model.NodeIterator;
 import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+
+import com.asydeo.ontology.Asydeo;
+
 
 public class OntView {
 	
@@ -43,6 +48,30 @@ public class OntView {
 		return i.getModel().shortForm(longuri);
 	}
 
+	// Get the label of this individual's class. Since calling
+	// the Individual.getOntClass() cannot guarantee which class
+	// may be returned, we extract the name from the individual's URI
+	public String getClassLabel() {
+	    String label = getType();
+
+	    String regex = Asydeo.PREFIX + ":\\w+";
+	    Matcher m = Pattern.compile(regex).matcher( getURI() );
+
+	    if ( m.find() ) {
+	        String classUri = getURI().substring(0, m.end());
+	        classUri = classUri.replace(Asydeo.PREFIX + ":", "");
+	        classUri = Asydeo.NS + classUri;
+	        
+	        OntResource resource = i.getOntModel().getOntResource(classUri);
+
+	        if ( resource != null ) {
+	            label = resource.getLabel(null);
+	        }
+	    }
+	    
+	    return label;
+	}
+	
 	public static OntView $(OntResource item) {
 		return new OntView(item);
 	}
